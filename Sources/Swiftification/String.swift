353bd6c832a1602273cb8d3282272bf2be1555ef
Stephan Heilner <stephanheilner@gmail.com>
2021-01-06 14:56:46 -0700

    Updated to support swift package manager

---
+++ Sources/Swiftification/String.swift
@@ -0,0 +1,82 @@
+//
+// Copyright (c) 2021 GreenJell0
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import Foundation
+import CommonCrypto
+
+public extension StringProtocol {
+    
+    /// Safely access the character at the given index, returning `nil` if `index` is out of bounds
+    /// This should be used in conjunction with indexes `advancedBy(_:limit)` to avoid crashes
+    subscript(safe index: Index) -> Character? {
+        return index < endIndex ? self[index] : nil
+    }
+    
+    /// Strips whitespaces from both the beginning and the end of self.
+    func trimmed() -> String {
+        return String(self).trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    /// Returns random string of length with specified characters.
+    static func random(_ length: Int = Int.random(8..<65), characters: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345789") -> String {
+        if characters.isEmpty { return "" }
+        
+        var result = String()
+        for _ in 0..<length {
+            result.append(characters[characters.index(characters.startIndex, offsetBy: Int.random(0..<characters.count))])
+        }
+        return result
+    }
+    
+    /// Returns a value with string inserted at index.
+    func inserting(_ string: String, at i: String.IndexDistance) -> String {
+        var str = String(self)
+        let index = str.index(str.startIndex, offsetBy: i, limitedBy: str.endIndex) ?? str.endIndex
+        str.insert(contentsOf: string, at: index)
+        return str
+    }
+    
+    func md5() -> String {
+        let context = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
+        var digest = Array<UInt8>(repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        CC_MD5_Init(context)
+        CC_MD5_Update(context, String(self), CC_LONG(lengthOfBytes(using: .utf8)))
+        CC_MD5_Final(&digest, context)
+        context.deallocate()
+        return digest.map { String(format:"%02x", $0) }.joined()
+    }
+    
+    var range: Range<String.Index> {
+        return startIndex..<endIndex
+    }
+    
+    var nsRange: NSRange {
+        return NSRange(range, in: self)
+    }
+    
+}
+
+public func + (lhs: String?, rhs: String?) -> String? {
+    guard let lhs = lhs else { return rhs }
+    guard let rhs = rhs else { return lhs }
+    return lhs + rhs
+}
--- Swiftification.podspec
+++ Swiftification.podspec
@@ -1,15 +1,19 @@
 Pod::Spec.new do |s|
   s.name         = "Swiftification"
-  s.version      = "11.1.0"
+  s.version      = "12.0.0"
   s.summary      = "Swift extensions to make life more pleasant."
   s.authors      = 'Hilton Campbell', 'Stephan Heilner', 'Branden Russell', 'Nick Shelley', 'Rhett Rogers'
   s.homepage     = "https://github.com/GreenJell0/Swiftification"
   s.license      = { :type => 'MIT', :file => 'LICENSE' }
   s.source       = { :git => "https://github.com/GreenJell0/Swiftification.git", :tag => s.version.to_s }
-  s.ios.deployment_target = '10.0'
+  s.ios.deployment_target = '12.0'
   s.osx.deployment_target = '10.11'
-  s.tvos.deployment_target = '10.0'
-  s.source_files = 'Swiftification/*.swift'
+  s.tvos.deployment_target = '12.0'
+  s.source_files = 'Sources/Swiftification/*.swift'
   s.requires_arc = true
   s.swift_version = '5.0'
+  
+  s.test_spec 'Tests' do |t|
+    t.source_files = 'Tests/SwiftificationTests/*.{swift}'
+  end
 end
